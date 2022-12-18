b80f147b1aaac40db92e5878eaa343c2d1054a11
umlx5h <umlx5h21@protonmail.com>
2022-12-18 10:56:26 +0000

    29 thread 1

---
+++ 29-thread-basic/simple_thread.c
@@ -0,0 +1,36 @@
+#include <pthread.h>
+#include "tlpi_hdr.h"
+
+static void *
+threadFunc(void *arg)
+{
+    char *s = (char *) arg;
+    printf("%s", s);
+
+    // stop("sub");
+    // equivelent
+    // pthread_exit((void *) strlen(s));
+    return (void *) strlen(s);
+}
+
+int
+main(int argc, char *argv[])
+{
+    pthread_t t1;
+    void *res;
+    int s;
+
+    s = pthread_create(&t1, NULL, threadFunc, "Hello world\n");
+    if (s != 0)
+        errExitEN(s, "pthread_create");
+    
+    printf("Message from main()\n");
+    // stop("main");
+    s = pthread_join(t1, &res);
+    if (s != 0)
+        errExitEN(s, "pthread_join");
+
+    printf("Thread returned %ld\n", (long) res);
+
+    exit(EXIT_SUCCESS);
+}
