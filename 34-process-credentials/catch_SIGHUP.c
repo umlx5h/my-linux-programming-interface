2050ede8fda11dd632799b7aa6d0c54388e7e447
umlx5h <umlx5h21@protonmail.com>
2023-04-04 00:38:08 +0000

    34 process credentials 3

---
+++ 34-process-credentials/catch_SIGHUP.c
@@ -0,0 +1,43 @@
+#define _XOPEN_SOURCE 500
+#include <unistd.h>
+#include <signal.h>
+#include "tlpi_hdr.h"
+
+static void
+handler(int sig)
+{
+}
+
+int
+main(int argc, char *argv[])
+{
+    pid_t childPid;
+    struct sigaction sa;
+
+    setbuf(stdout, NULL);
+
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = handler;
+    if (sigaction(SIGHUP, &sa, NULL) == -1)
+        errExit("sigaction");
+    
+    childPid = fork();
+    if (childPid == -1)
+        errExit("fork");
+    
+    if (childPid == 0 && argc > 1) 
+        if (setpgid(0, 0) == -1) /* Move 'child' process to new process group */
+            errExit("setpgid");
+    
+    printf("%s PID=%ld; PPID=%ld; PGID=%ld; SID=%ld\n",
+        childPid == 0 ? "Child " : "Parent",
+        (long) getpid(), (long) getppid(), (long) getpgrp(), (long) getsid(0));
+    alarm(60);         /* An unhandled SIGALRM ensures this process
+                            will die if nothing else terminates it */
+    
+    for (;;) {        /* Wait for signals */
+        pause();
+        printf("%ld: caught SIGHUP\n", (long) getpid());
+    }
+}
